427b9efddcc432b15f2bfd243bbe397a3e55b23f
medo <medo@gmail.com>
2024-12-03 09:51:50 +0100

    add articles items and add some js classes to handle data

---
+++ js/model/articles.js
@@ -0,0 +1,21 @@
+
+class articles {
+
+  var id;
+  var title;
+  var label;
+  var items;
+  var content;
+
+
+  constructor(id,title,label,items,content) {
+    this.id=id;
+    this.title=title;
+    this.label=label;
+    this.items=items;
+    this.content=content;
+
+  }
+
+  
+}
